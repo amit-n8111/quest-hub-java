e195a169c097fa35c9b4c7fde3782b615370b98e
Ashutosh Srivastav <Ashutosh Srivastav@DESKTOP-UD5RRH4>
2019-08-21 15:36:39 +0530

    auto generated sequence in a thread safe way

--- quest-hub/src/main/java/com/citi/quest/api/event/handlers/ApplicationEventHandler.java
+++ quest-hub/src/main/java/com/citi/quest/api/event/handlers/ApplicationEventHandler.java
@@ -1,9 +1,5 @@
 package com.citi.quest.api.event.handlers;
 
-import java.util.Comparator;
-import java.util.List;
-
-import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.HandleBeforeSave;
@@ -12,39 +8,37 @@ import org.springframework.stereotype.Component;
 
 import com.citi.quest.api.domain.Application;
 import com.citi.quest.api.repositories.ApplicationRepository;
+import com.citi.quest.api.util.SequenceGenerator;
 
 @RepositoryEventHandler(Application.class)
 @Component
 public class ApplicationEventHandler {
-	
+	/** Unique sequence key for Application collection. */
+	private static final String APPLICATION_SEQ_KEY = "APPLICATION_SEQUENCE";
+
 	@Autowired
-	ApplicationRepository applicationRepository;
+	private ApplicationRepository applicationRepository;
+
+	@Autowired
+	private SequenceGenerator sequenceGenerator;
 	
 	@HandleBeforeSave
 	public void handleApplicationSaveWithoutId(Application application) {
-		Long maxId = 0L;
 		if (application.getId() <= 0 || application.getId() == null) {
-			List<Application> applications = applicationRepository.findAll();
-			if(CollectionUtils.isNotEmpty(applications)) {
-				Application max = applications.stream().max(Comparator.comparing(Application::getId)).get();
-				maxId=max.getId();
-			}
-			application.setId(maxId + 1);
+			Long nextSequence = sequenceGenerator.getNextSequence(APPLICATION_SEQ_KEY);
+			application.setId(nextSequence);
 		}
 		applicationRepository.save(application);
 	}
 	
 	@HandleBeforeCreate
 	public void handleApplicationCreateWithoutId(Application application) {
-		Long maxId = 0L;
+
 		if (null == application.getId() || application.getId() <= 0) {
-			List<Application> applications = applicationRepository.findAll();
-			if(CollectionUtils.isNotEmpty(applications)) {
-				Application max = applications.stream().max(Comparator.comparing(Application::getId)).get();
-				maxId=max.getId();
-			}
-			application.setId(maxId + 1);
+			Long nextSequence = sequenceGenerator.getNextSequence(APPLICATION_SEQ_KEY);
+			application.setId(nextSequence);
 		}
+
 		applicationRepository.save(application);
 	}
 
---
+++ quest-hub/src/main/java/com/citi/quest/api/util/SequenceGenerator.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package com.citi.quest.api.util;
+
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import com.citi.quest.api.domain.Sequence;
+
+/**
+ * Service class responsible for generating unique sequence for insert
+ * operations of an document.
+ * 
+ * @author Ashutosh Srivastav
+ * @createdOn Aug 21, 2019
+ */
+@Service
+public class SequenceGenerator {
+
+	/** Logger for class. */
+	private Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+	/** Name of _id field in the collection. */
+	private static final String FIELD_ID = "sequenceName";
+
+	/** Name of field representing sequence counter. */
+	private static final String FIELD_NEXT_SEQ = "nextSequence";
+
+	@Autowired
+	private MongoOperations mongoOperations;
+
+	/**
+	 * Returns the next available sequence to be assigned for insert operations.
+	 * 
+	 * @param domainSequenceKey - a key uniquely representing a domain object
+	 * @return the next available sequence as <Long>
+	 */
+	public Long getNextSequence(String domainSequenceKey) {
+		LOGGER.info("Entry");
+
+		Criteria criteria = Criteria.where(FIELD_ID).is(domainSequenceKey);
+		Query query = new Query(criteria);
+
+		Update update = new Update();
+		update.inc(FIELD_NEXT_SEQ, 1);
+
+		FindAndModifyOptions options = FindAndModifyOptions.options()
+				.returnNew(true) //return the updated doc
+				.upsert(true); //create for the first time
+
+		// update the counter in a thread safe way
+		Sequence sequence = mongoOperations.findAndModify(query, update, options, Sequence.class);
+		Long nextSequence = sequence.getNextSequence();
+
+		LOGGER.info("Returning next sequence: {} for key: {}", nextSequence, domainSequenceKey);
+		return nextSequence;
+	}
+
+}
